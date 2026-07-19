6074b2c96e2b3198fb1988a6d4cd88c9cea81619
Moustafa Mohamed <111659887+Darsh2984@users.noreply.github.com>
2026-07-19 16:04:50 +0300

    Adjust auth

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { Platform } from "react-native";
+import { getToken } from "./storage";
 
 function getApiBaseUrl() {
   const configuredUrl =
@@ -23,5 +24,22 @@ const api = axios.create({
     Accept: "application/json",
   },
 });
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = await getToken();
+
+    if (token) {
+      config.headers =
+        config.headers || {};
+
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
